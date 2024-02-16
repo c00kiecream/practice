884e5f5401b8bef16575a0835663024d889a39a2
DESKTOP-BI5BBNV\hi <stv9450@gmail.com>
2024-02-16 11:00:50 +0900

    first commit

---
+++ src/main/java/org/example/ohgiraffers/githubpractice01/GithubPractice01Application.java
@@ -0,0 +1,13 @@
+package org.example.ohgiraffers.githubpractice01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPractice01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubPractice01Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ohgiraffers/githubpractice01/GithubPractice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ohgiraffers.githubpractice01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPractice01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
